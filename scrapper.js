d04df4747e3e363832059f69a25b89b1eb80717d
Bartosz Bączek (bartbac177) <bartbac177@student.polsl.pl>
2017-05-14 11:13:08 +0200

    scrapper checks first for response number. If number != 200 it proceeds to next song

--- scrapper.js
+++ scrapper.js
@@ -4,28 +4,35 @@ var casper = require('casper').create({
     logLevel: "debug"
 });
 
+
 var SESSION_URL = 'https://thesession.org/tunes/';
 var FIRST_PAGE_INDEX = 1;
 var NUMBER_OF_PAGES = 16000;
 
+var currentPageIndex = FIRST_PAGE_INDEX;
+var numberOf4xxResponses = 0;
+
+
 casper.start(SESSION_URL);
 
-var currentPageIndex = FIRST_PAGE_INDEX;
+casper.repeat(NUMBER_OF_PAGES, function () {
 
-casper.repeat(NUMBER_OF_PAGES, function() {
-    
-    casper.thenOpen(SESSION_URL + currentPageIndex, function() {
-        
-        casper.waitForSelector('.notes', function(){
+    casper.thenOpen(SESSION_URL + currentPageIndex, function (response) {
 
-            var songs = casper.fetchText('.notes');            
-            file.appendTo('log.txt', songs);
-
-        });
-
+        if (response['status'] !== 200) {
+            numberOf4xxResponses++;
+        } else {
+            casper.waitForSelector('.notes', function(){
+                var songs = casper.fetchText('.notes');            
+                file.appendTo('log.txt', songs);
+            });
+        }
     });
 
     currentPageIndex++;
+}).then(function() {
+    console.log('Thesession.org is scrapped')
+    console.log('Number of 4xx responses: ', numberOf4xxResponses);
 });
 
 casper.run();
